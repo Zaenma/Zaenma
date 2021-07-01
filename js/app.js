e767742ff6f9951bc2c16a81e5b1669b7719bb8f
Zaenma <zaenma.halidisalim@gmail.com>
2021-07-01 20:07:38 +0000

    jgdfkgfjghfjgj

--- js/app.js
+++ js/app.js
@@ -12,60 +12,60 @@ const image = document.querySelector('.image');
 const liens = document.getElementsByClassName('lien');
 const pages = document.getElementsByClassName('page');
 
+const contact = document.getElementById('contact');
+const btn = document.querySelector('.btn');
 
-for (let i = 0; i < liens.length; i++) {
-  const lien = liens[i];
-  
-  lien.addEventListener('click', (event) => {
-    
-    for (let j = 0; j < pages.length; j++) 
-    {
-      const page = pages[j]; 
-      if (page.attributes['id'].value === lien.attributes['id'].value) {
-        event.preventDefault();
-        page.classList.remove('hidden');
-        home.classList.add("hidden");
-        if (page.attributes['id'].value !== "home") {
-          image.classList.add('hidden');
-        }else{
-          image.classList.remove('hidden');
-        }
-        container.classList.remove("active");
+const mai_container = document.querySelector('.main-container');
+
+// bule = mai_container.createElement('span');
+console.log(mai_container);
+
+function btn_contact() {
+  btn.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    for (let i = 0; i < pages.length; i++) {
+      const page = pages[i];
+      if (page.attributes['id'].value !== "contact") {
+        page.classList.add('hidden');
+        image.classList.add('hidden');
       }else{
-        page.classList.add("hidden");
+        page.classList.remove('hidden');
       }
     }
   })
-  
 }
 
-// const titreSpans = document.querySelectorAll('h1 span');
-// const btns = document.querySelectorAll('.btn-first');
-// const logo = document.querySelector('.logo');
-const medias = document.querySelectorAll('.bulle');
-// const l1 = document.querySelector('.l1');
-// const l2 = document.querySelector('.l2');
+btn_contact();
 
-window.addEventListener('load', () => {
-
-    const TL = gsap.timeline({paused: true});
-
-    TL
-    // .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
-    // .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
-    // .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
-    .from(image, 1, {width: 0, ease: "power2.out"}, '-=2')
-    // .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, '-=2')
-    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
-
-    
-    
-
-    TL.play();
-})
-
-
-// Typing 
+function menu_click() {
+  
+  for (let i = 0; i < liens.length; i++) {
+    const lien = liens[i];
+    lien.addEventListener('click', (event) => {
+      for (let j = 0; j < pages.length; j++) 
+      {
+        const page = pages[j]; 
+        if (page.attributes['id'].value === lien.attributes['id'].value) {
+          event.preventDefault();
+          page.classList.remove('hidden');
+          home.classList.add("hidden");
+          if (page.attributes['id'].value !== "home") {
+            image.classList.add('hidden');
+          }else{
+            image.classList.remove('hidden');
+          }
+          container.classList.remove("active");
+        } else{
+          page.classList.add("hidden");
+        }
+      }
+    })
+  }
+}
+  
+menu_click();
+  // Typing 
 
 const typedTextSpan = document.querySelector(".infos");
 const cursorSpan = document.querySelector(".cursor");
